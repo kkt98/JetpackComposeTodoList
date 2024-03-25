b96b4d61dd2d5b6220ddb961e8bf5dfe08899966
kkt98 <sux1011@naver.com>
2024-03-26 02:14:36 +0900

    2024 03 26 Jetpack-Compose-UI

---
+++ AndroidTP/ToDoList2/app/src/main/java/com/example/todolist/Data.kt
@@ -0,0 +1,26 @@
+package com.example.todolist
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CropSquare
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Event
+import androidx.compose.material.icons.filled.PrivacyTip
+import androidx.compose.material.icons.filled.RestoreFromTrash
+import androidx.compose.ui.graphics.vector.ImageVector
+import java.util.UUID
+
+enum class ToDoIcons(val imageVector: ImageVector,@StringRes contentDescription: Int) {
+    Square(Icons.Default.CropSquare, R.string.square),
+    Done(Icons.Default.Done, R.string.done),
+    Event(Icons.Default.Event, R.string.event),
+    Privacy(Icons.Default.PrivacyTip, R.string.privacy),
+    Trash(Icons.Default.RestoreFromTrash, R.string.trash)
+
+
+}
+class Data (
+    val task: String,
+    val icons: ToDoIcons = ToDoIcons.Square,
+    val id: UUID = UUID.randomUUID()
+)
---
+++ AndroidTP/ToDoList2/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolist.ui.theme.ToDoListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            ToDoListTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+//뷰
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+//미리보기
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToDoListTheme {
+        Greeting("Android")
+    }
+}
