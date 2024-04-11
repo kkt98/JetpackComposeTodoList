e9db3ec50fb0cb84722b22e6b46af60a0ac4e95a
kkt98 <sux1011@naver.com>
2024-04-12 02:47:51 +0900

    2024 04 11 JetPackCompose TodoList

--- AndroidTP/TodoListFromJetpackCompose/app/src/main/java/com/example/todolistfromjetpackcompose/screens/CalenderScreen.kt
+++ AndroidTP/TodoListFromJetpackCompose/app/src/main/java/com/example/todolistfromjetpackcompose/screens/CalenderScreen.kt
@@ -1,30 +1,24 @@
 package com.example.todolistfromjetpackcompose.screens
 
-import android.os.Build
-import android.util.Log
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,36 +26,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.todolistfromjetpackcompose.ui.theme.TodoListFromJetpackComposeTheme
+import com.example.todolistfromjetpackcompose.room.PlanEntity
 import com.example.todolistfromjetpackcompose.viewmodel.CalenderPlanViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.boguszpawlowski.composecalendar.SelectableCalendar
-import io.github.boguszpawlowski.composecalendar.StaticCalendar
-import io.github.boguszpawlowski.composecalendar.day.DayState
 import io.github.boguszpawlowski.composecalendar.rememberSelectableCalendarState
-import io.github.boguszpawlowski.composecalendar.selection.DynamicSelectionState
 import io.github.boguszpawlowski.composecalendar.selection.SelectionMode
-import java.time.LocalDate
-import java.time.YearMonth
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun CalenderScreen(viewModel: CalenderPlanViewModel = hiltViewModel()) {
-
     val context = LocalContext.current
     val saveSuccess by viewModel.saveSuccess.collectAsState()
     val schedules by viewModel.schedules.collectAsState()
-
-    LaunchedEffect(saveSuccess) {
-        if (saveSuccess) {
-            Toast.makeText(context, "저장 성공", Toast.LENGTH_SHORT).show()
-        }
-    }
 
     val calendarState = rememberSelectableCalendarState(
         initialSelectionMode = SelectionMode.Single,
@@ -69,20 +46,13 @@ fun CalenderScreen(viewModel: CalenderPlanViewModel = hiltViewModel()) {
     var showDialog by remember { mutableStateOf(false) }
     val selectedDate = calendarState.selectionState.selection.firstOrNull()
 
-
-    LaunchedEffect(selectedDate) {
-        selectedDate?.let {
-            viewModel.getSchedulesByDate(it.toString())
-        }
-    }
-
-    schedules.forEach {
-        Log.d("Asdasdasd", it.plan)
-    }
+    viewModel.getSchedulesByDate(
+        calendarState.selectionState.selection.joinToString { it.toString() }
+    )
 
     Scaffold(
         floatingActionButton = {
-            if (selectedDate != null) { // 날짜가 선택되었을 때만 버튼 표시
+            if (selectedDate != null) {
                 FloatingActionButton(onClick = { showDialog = true }) {
                     Icon(Icons.Filled.Add, contentDescription = "Add")
                 }
@@ -90,13 +60,12 @@ fun CalenderScreen(viewModel: CalenderPlanViewModel = hiltViewModel()) {
         }
     ) {
         Column(
-            Modifier
-                .verticalScroll(rememberScrollState())
-                .padding(it)
+            modifier = Modifier.padding(it)
         ) {
-            SelectableCalendar(
-                calendarState = calendarState,
-            )
+            SelectableCalendar(calendarState = calendarState)
+            if (selectedDate != null) {
+                SchedulesList(schedules)
+            }
 
             if (showDialog) {
                 ScheduleDialog(
@@ -107,6 +76,34 @@ fun CalenderScreen(viewModel: CalenderPlanViewModel = hiltViewModel()) {
                     }
                 )
             }
+        }
+    }
+}
+
+
+@Composable
+fun SchedulesList(schedules: List<PlanEntity>) {
+    LazyColumn {
+        items(schedules) { schedule ->
+            ScheduleItem(schedule)
+        }
+    }
+}
+
+@Composable
+fun ScheduleItem(schedule: PlanEntity) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = 2.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Date: ${schedule.date}", style = MaterialTheme.typography.subtitle1)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("Plan: ${schedule.plan}", style = MaterialTheme.typography.body1)
         }
     }
 }
@@ -148,30 +145,5 @@ fun ScheduleDialog(
                 Text("취소")
             }
         }
-    )
-}
-
-@Composable
-private fun SelectionControls(
-    selectionState: DynamicSelectionState,
-) {
-    Text(
-        text = "Calendar Selection Mode",
-        style = MaterialTheme.typography.h5,
-    )
-    SelectionMode.values().forEach { selectionMode ->
-        Row(modifier = Modifier.fillMaxWidth()) {
-            RadioButton(
-                selected = selectionState.selectionMode == selectionMode,
-                onClick = { selectionState.selectionMode = selectionMode }
-            )
-            Text(text = selectionMode.name)
-            Spacer(modifier = Modifier.height(4.dp))
-        }
-    }
-
-    Text(
-        text = "Selection: ${selectionState.selection.joinToString { it.toString() }}",
-        style = MaterialTheme.typography.h6,
     )
 }
